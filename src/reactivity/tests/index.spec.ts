cb8869b59ebcc7a51c4a159d446d53209bd0f9d8
dylan <l>
2022-01-22 13:10:46 +0800

    a03:安装babel，为了能使用import

--- src/reactivity/tests/index.spec.ts
+++ src/reactivity/tests/index.spec.ts
@@ -1,4 +1,12 @@
+import {add} from '../index'
 it('init',()=>{
     console.log('test')
     expect(true).toBe(true)
+})
+
+
+it('add',()=>{
+    console.log('babel')
+
+    expect(add(1,2)).toBe(3)
 })
