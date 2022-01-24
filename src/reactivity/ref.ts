943bcf870b781398150524fe2a9ad67da0d08a35
dylan <l>
2022-01-24 18:29:42 +0800

    d03:ref() 接受 obj 的实现

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,13 +1,16 @@
 import {trackEffects,triggerEffects} from './effect'
-import {hasChanged} from "../../shared";
+import {hasChanged,isObject} from "../../shared";
+import {reactive} from "./reactive";
 
 class RefImp{
+    private _rawValue: any;
     private _value: any;
     public dep
     // effect.ts 的 track 是 reactive对象专属的，所以，effect.ts 的 objMap 是 reactive对象专属的
     // 所有 reactive对象 收集来的 effect 都会集中地储存在 集中的objMap中
     constructor(value){
-        this._value = value
+        this._rawValue = value
+        this._value = convertToReactive(value)
         this.dep = new Set() // 而 ref 的 dep 则分散地储存在自己这儿
     }
 
@@ -20,12 +23,17 @@ class RefImp{
     set value(value){
         if(!hasChanged(value,this._value))return
 
-        this._value = value
+        this._rawValue = value
+        this._value = convertToReactive(value)
 
         triggerEffects(this.dep)
     }
 }
 
+function convertToReactive(value){
+    return isObject(value) ?  reactive(value) : value
+
+}
 
 export function ref(value) {
     return new RefImp(value)
