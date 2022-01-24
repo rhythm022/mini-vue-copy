7aba35b1d8a6076f896710653e48e6a4f9ae8cc3
dylan <l>
2022-01-24 18:07:43 +0800

    d02:ref 的雏形实现

---
+++ src/reactivity/ref.ts
@@ -0,0 +1,30 @@
+import {trackEffects,triggerEffects} from './effect'
+class RefImp{
+    private _value: any;
+    public dep
+    // effect.ts 的 track 是 reactive对象专属的，所以，effect.ts 的 objMap 是 reactive对象专属的
+    // 所有 reactive对象 收集来的 effect 都会集中地储存在 集中的objMap中
+    constructor(value){
+        this._value = value
+        this.dep = new Set() // 而 ref 的 dep 则分散地储存在自己这儿
+    }
+
+    get value(){
+        trackEffects(this.dep)
+
+        return this._value
+    }
+
+    set value(value){
+        if(Object.is(value,this._value)) return
+
+        this._value = value
+
+        triggerEffects(this.dep)
+    }
+}
+
+
+export function ref(value) {
+    return new RefImp(value)
+}
---
+++ src/reactivity/tests/ref.spec.ts
@@ -0,0 +1,31 @@
+import { effect } from '../effect'
+import { ref } from '../ref'
+
+describe('ref',()=>{
+    it('happy path',()=>{
+        const a = ref(1)
+        expect(a.value).toBe(1)
+    })
+
+    it('should be reactive',()=>{
+        const a = ref(1)
+        let dummy
+        let calls = 0
+        effect(()=>{
+            calls++
+            dummy = a.value
+        })
+        expect(calls).toBe(1)
+        expect(dummy).toBe(1)
+
+        //
+        a.value = 3
+        expect(calls).toBe(2)
+        expect(dummy).toBe(3)
+
+        // same value should not trigger
+        a.value = 3
+        expect(calls).toBe(2)
+
+    })
+})
